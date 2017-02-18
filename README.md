c0e021c4fb573ef1a3e9859cb68acef8f74ef93e
Ritesh Shrivastav <ritesh_shrv@live.com>
2017-02-18 20:16:56 +0530

    Boilerplate for basic python module

---
+++ README.md
@@ -0,0 +1,13 @@
+# Extract Nifty50 from NSE India
+Python module to extract data from NSE India for Nifty50
+
+## Requirements & Installation
+
+Clone the repository and then install it.
+Package requirements are handled using pip. To install them do
+
+```
+pip install -r requirements.txt
+```
+
+## Examples
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+test_requirements = []
+
+setup(
+    name='nifty50-scraper',
+    version='0.1.1',
+    description="Python module to extract data from NSE India for Nifty50",
+    long_description=readme,
+    author="Ritesh Shrivastav",
+    author_email='ritesh_shrv@live.com',
+    url='',
+    packages=[
+        'nifty50',
+    ],
+    package_dir={'nifty50': 'nifty50'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    keywords='nifty50-scraper',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
